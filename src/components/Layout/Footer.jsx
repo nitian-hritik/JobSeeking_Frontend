4ba49e103257c1c8579be3a43ea0e0646cd3f468
nitian-hritik <rajeshkumishra40@gmail.com>
2024-04-03 21:11:48 +0530

    1st commit

---
+++ src/components/Layout/Footer.jsx
@@ -0,0 +1,17 @@
+import React, { useContext } from "react";
+import { Context } from "../../main";
+import { Link } from "react-router-dom";
+// import { FaFacebookF, FaYoutube, FaLinkedin } from "react-icons/fa";
+// import { RiInstagramFill } from "react-icons/ri";
+
+const Footer = () => {
+  const { isAuthorized } = useContext(Context);
+  return (
+    <footer className={isAuthorized ? "footerShow" : "footerHide"}>
+      <div>&copy; All Rights Reserved By Hritik Roshan Kumar Mishra.</div>
+      
+    </footer>
+  );
+};
+
+export default Footer;
